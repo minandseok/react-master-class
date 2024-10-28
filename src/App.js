853aa7ed403d1e294893de113edd7c3d4ca1d9d0
minandseok <jeonminseok758@gmail.com>
2024-10-28 18:29:43 +0900

    finished #2

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vw;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
+  );
+}
+
+export default App;
