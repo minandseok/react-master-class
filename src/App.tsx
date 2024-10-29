ce5d354e46f706f91f83c035bc17b0ca67f3c461
minandseok <jeonminseok758@gmail.com>
2024-10-30 00:54:05 +0900

    finished #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import Circle from "./Circle";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor}
+`
+
+const Btn = styled.button`
+  color: ${(props) => props.theme.btnColor}
+`
 
 function App() {
   return (
-    <div>
-       <Circle borderColor="yellow" bgColor="teal" />
-       <Circle text="Hi" bgColor="tomato" />
-    </div>
+    <Container>
+      <H1>Hello</H1>
+      <Btn>Click!</Btn>
+    </Container>
   );
 }
 
