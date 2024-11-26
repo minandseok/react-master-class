f336d5215a53fcb9f1cb28df5c0d28f3d15528a8
minandseok <jeonminseok758@gmail.com>
2024-11-26 22:52:50 +0900

    finished 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 function App() {
-  return null;
+  return (
+    <Container>
+      <H1>Hello</H1>
+    </Container>
+  );
 }
 
 export default App;
