504933372b3a61b21ec168a2946341cfc536ebec
minandseok <jeonminseok758@gmail.com>
2024-12-03 00:19:00 +0900

    finished 5.3

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./theme";
+import { theme } from "./theme";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,8 @@
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
-};
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "white",
-  btnColor: "teal",
+export const theme: DefaultTheme = {
+  bgColor: "#778da9",
+  textColor: "#ffb703",
+  btnColor: "#fb8500",
+  accentColor: "#023047",
 };
